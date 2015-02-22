eb5f6ba11ac8b1df3f7182f44f275faadfec5729
KRVPerera <rukshan.viduranga@gmail.com>
2015-02-23 02:06:49 +0530

    added comments for git proxy scripts

--- bash_scripts/git_set_proxy.sh
+++ bash_scripts/git_set_proxy.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This script is specially designed for my university 
+# proxy settings
 git config --global http.proxy http://cache.mrt.ac.lk:3128
 git config --global https.proxy https://cache.mrt.ac.lk:3128
 git config --global -l
